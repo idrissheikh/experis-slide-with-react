0a136d4f7ef76902b4de725d46cdafdd1913460f
idrissheikh-idris <s310058@oslomet.no>
2023-01-13 01:14:41 +0100

    first commit

---
+++ src/components/Slide.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Slide.css'
+
+function Slide() {
+
+
+
+  return (
+    <div className="slide">
+        <div className="bcg1">
+            <div className="bcg2">
+                <div className="txt">
+                    <h1>Rådgivning, utvikling og forvaltning av løsninger som forenkler morgendagen</h1>
+                    <p>Hos oss finner du mennesker som ikke bare har den tekniske kompetansen som skal til for å lykkes med et digitaliseringsprosjekt, 
+                        men også de riktige personlige egenskapene.</p>
+                </div>
+
+            
+
+            </div>
+           
+          
+        </div>
+    </div>
+  )
+}
+
+export default Slide;
